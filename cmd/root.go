202fc2e76139bfd27cd65e57991e0db795d0eaf6
ken <ynkn0829@gmail.com>
2022-08-26 13:11:46 +0000

    Add title

--- cmd/root.go
+++ cmd/root.go
@@ -1,3 +1,11 @@
+//
+//  root.go
+//  go-cli
+//
+//  Created by Kentaro Yano on 2022/08/26.
+//  Copyright © 2022 InterPark. All rights reserved.
+//
+
 package cmd
 
 import (
--- main.go
+++ main.go
@@ -1,3 +1,11 @@
+//
+//  main.go
+//  go-cli
+//
+//  Created by Kentaro Yano on 2022/08/26.
+//  Copyright © 2022 InterPark. All rights reserved.
+//
+
 package main
 
 import "go-cli/cmd"
