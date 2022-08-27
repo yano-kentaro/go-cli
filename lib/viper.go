c2328899db5bbe91e73d2c07db7c52a5f5f02517
ken <ynkn0829@gmail.com>
2022-08-27 06:02:48 +0000

    Add viper lib

---
+++ lib/viper.go
@@ -0,0 +1,17 @@
+//
+//  viper.go
+//  go-cli
+//
+//  Created by Kentaro Yano on 2022/08/27.
+//  Copyright © 2022 InterPark. All rights reserved.
+//
+
+package lib_viper
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
